a09063a154f36533127fe8db3cbde97845b009a2
demon00 <dfasterdriver@yandex.ru>
2017-05-25 12:22:45 +0300

    was added file for desktop

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,15 @@
 $('.main__car-slider').slick({
   autoplay: true,
-  autoplaySpeed: 9000,
+  autoplaySpeed: 6000,
+  speed: 1000,
+  dots: false,
+  arrows: true,
+  draggable: false
+});
+
+$('.partners_slider').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
   speed: 1000,
   dots: false,
   arrows: true,
